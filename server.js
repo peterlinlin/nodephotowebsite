50b508bddcd549423229965352da157eb05a4ceb
Peter Lin <peterlin.california@gmail.com>
2020-09-12 16:48:31 -0700

    linked all the pages together

--- server.js
+++ server.js
@@ -6,14 +6,41 @@ app.use(express.json());
 app.use(express.static("express"));
 app.use(express.static('public'));
 // default URL for website
+/*
 app.use('/', function(req,res){
+    res.sendFile(path.join(__dirname+'/express/pages/home.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+
+app.use('./about', function(req,res){
+    res.sendFile(express.static(__dirname+'/express/pages/about.html'));
+    //__dirname : It will resolve to your project folder.
+  });
+  */
+
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/pages/home.html'));
+});
+
+app.get('/home', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/pages/home.html'));
+});
+
+app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname+'/express/pages/about.html'));
-    //__dirname : It will resolve to your project folder.
-  });
-app.use('/about', function(req,res,html){
-    res.sendFile(express.static(__dirname+'/pages/about.html'));
-    //__dirname : It will resolve to your project folder.
-  });
+});
+
+app.get('/bananas', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/pages/bananas.html'));
+});
+
+app.get('/contactme', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/pages/contactme.html'));
+});
+
+app.get('/gallery', (req, res) => {
+    res.sendFile(path.join(__dirname+'/express/pages/gallery.html'));
+});
 
 const server = http.createServer(app);
 const port = 3000;
