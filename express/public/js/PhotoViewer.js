5236058c19c3b5fd0078d129080960d07592ccd7
Peter Lin <peterlin.california@gmail.com>
2020-09-13 14:35:31 -0700

    made it so photos are normal sized instead of small

--- express/public/js/PhotoViewer.js
+++ express/public/js/PhotoViewer.js
@@ -79,9 +79,11 @@ function viewAlbum(albumName) {
       var photoUrl = bucketUrl + encodeURIComponent(photoKey);
       return getHtml([
         '<span>',
-          '<div>',
+          "<div class='col-sm-4'>",
             '<br/>',
-            '<img style="width:128px;height:128px;" src="' + photoUrl + '"/>',
+            "<figure class='photo-frame'>",
+            "<img src=' " + photoUrl + "' class='gallery'/>",
+            "</figure>",
           '</div>',
           '<div>',
             '<span>',
@@ -100,6 +102,73 @@ function viewAlbum(albumName) {
           'Back To Albums',
         '</button>',
       '</div>',
+      '<h2>',
+        'Album: ' + albumName,
+      '</h2>',
+      message,
+      '<div>',
+        getHtml(photos),
+      '</div>',
+      '<h2>',
+        'End of Album: ' + albumName,
+      '</h2>',
+      '<div>',
+        '<button onclick="listAlbums()">',
+          'Back To Albums',
+        '</button>',
+      '</div>',
+    ]
+    document.getElementById('viewer').innerHTML = getHtml(htmlTemplate);
+    document.getElementsByTagName('img')[0].setAttribute('style', 'display:none;');
+  });
+}
+
+
+
+function viewMainAlbum(albumName) {
+  albumName = 'main';
+  var albumPhotosKey = encodeURIComponent(albumName) + '/';
+  s3.listObjects({Prefix: albumPhotosKey}, function(err, data) {
+    if (err) {
+      return alert('There was an error viewing your album: ' + err.message);
+    }
+    // 'this' references the AWS.Response instance that represents the response
+    var href = this.request.httpRequest.endpoint.href;
+    var bucketUrl = href + albumBucketName + '/';
+
+    var photos = data.Contents.map(function(photo) {
+      var photoKey = photo.Key;
+      var photoUrl = bucketUrl + encodeURIComponent(photoKey);
+      return getHtml([
+        '<span>',
+          "<div class='col-sm-4'>",
+            '<br/>',
+            "<figure class='photo-frame'>",
+            "<img src=' " + photoUrl + "' class='gallery'/>",
+            "</figure>",
+          '</div>',
+          '<div>',
+            '<span>',
+              photoKey.replace(albumPhotosKey, ''),
+            '</span>',
+          '</div>',
+        '</span>',
+      ]);
+    });
+
+
+
+    var message = photos.length ?
+      '<p>The following photos are present.</p>' :
+      '<p>There are no photos in this album.</p>';
+    var htmlTemplate = [
+      '<div>',
+        '<button onclick="listAlbums()">',
+          'Back To Albums',
+        '</button>',
+      '</div>',
+
+
       '<h2>',
         'Album: ' + albumName,
       '</h2>',
